f7aecc185463829c59bb74df18f67e7a26414776
Dagi <dagmawiamanuel@gmail.com>
2023-06-22 12:29:15 +0300

    added header.js file

---
+++ apple-react-only-html-css/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#"><img src="images/icons/logo.png" alt="" /></a>
+                    </li>
+                    <li><a href="#">Mac</a></li>
+                    <li><a href="#">iPad</a></li>
+                    <li><a href="#">iPhone</a></li>
+                    <li><a href="#">Watch</a></li>
+                    <li><a href="#">TV</a></li>
+                    <li><a href="#">Music</a></li>
+                    <li><a href="#">Support</a></li>
+                    <li>
+                        <a href="#"><img src="images/icons/search-icon.png" alt="" /></a>
+                    </li>
+                    <li>
+                        <a href="#"><img src="images/icons/cart.png" alt="" /></a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
